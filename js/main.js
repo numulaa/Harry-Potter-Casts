8a3d4a1eb72ae5edbd47c1523bc6e7257214ad0c
Nurul Mukhlisa <numulaa@gmail.com>
2022-09-16 14:32:31 +0700

    fixed cors errors

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-alert("working");
+document
+  .queerySelector("button")
+  .addEventListener("click", harryPotterAPIRequest);
+async function harryPotterAPIRequest() {
+  const harryPotterCastName = document.querySelector("input").value;
+  try {
+    const response = await fetch(
+      `https://harry-potter-casts-api.herokuapp.com/api/${harryPotterCastName}`
+    );
+    const data = await response.json();
+    console.log(data);
+    document.querySelector("#birthName").innerText = data.birthName;
+  } catch (error) {
+    console.log(error);
+  }
+}
