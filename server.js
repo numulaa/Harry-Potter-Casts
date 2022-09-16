d567185c6efd129b945cbac4eaf316e4803ea6e8
Nurul Mukhlisa <numulaa@gmail.com>
2022-09-16 14:12:48 +0700

    change the lowercase

--- server.js
+++ server.js
@@ -3,25 +3,25 @@ const app = express();
 const PORT = 8000;
 
 const hpCasts = {
-  "Harry Potter": {
+  "harry potter": {
     age: 33,
     birthName: "Daniel Jacob Radcliffe",
     birthDate: "23 July 1989",
     birthLocation: "London, England",
   },
-  "Hermione Granger": {
+  "hermione granger": {
     age: 32,
     birthName: "Emma Charlotte Duerre Watson",
     birthDate: "15 April 1990",
     birthLocation: "Paris, France",
   },
-  "Ron Weasley": {
+  "ron weasley": {
     age: 34,
     birthName: "Rupert Alexander Lloyd Grint",
     birthDate: "24 August 1988",
     birthLocation: "Harlow, Essex, England",
   },
-  "Draco Malfoy": {
+  "draco malfoy": {
     age: 34,
     birthName: "Thomas Andrew Felton",
     birthDate: "22 September 1987",
